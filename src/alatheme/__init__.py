98b67e30faf567a02fa0370259745a07d7c75e87
danielosw <65432065+danielosw@users.noreply.github.com>
2024-03-25 18:42:21 -0500

    add a helper function for fish completion and fix a bunch of bugs

--- src/alatheme/__init__.py
+++ src/alatheme/__init__.py
@@ -1,7 +1,9 @@
 import tomlkit
 from argparse import ArgumentParser
 from pathlib import Path
-
+from os import listdir
+from os.path import isfile, join
+import sys
 def main() -> None:
     parse = ArgumentParser(
             prog="Alatheme",
@@ -11,6 +13,9 @@ def main() -> None:
     theme = parse.parse_args()
     theme = theme.name
     fileopened: bool = False
+    if theme == "complete_fish":
+        print(list())
+        sys.exit()
     try:
         path = str(Path.home())+"/.config/alacritty/alacritty.toml"
         with open(path, "r+") as f:
@@ -38,6 +43,21 @@ def main() -> None:
             with open(path, "w+") as f:
                 f.write(tomlkit.dumps(toml))
         else:
-            raise FileNotFoundError("Unable to find"+theme+".toml")
+            raise FileNotFoundError("Unable to find "+theme+".toml")
     print("Successful edited file")
+def list() -> str:
+    try:
+        path = str(Path.home())+"/.config/alacritty/themes/"
+        themes = [f for f in listdir(path) if isfile(join(path, f))]
+    except FileNotFoundError:
+        try:
+            path = str(Path.home())+"/appadata/local/alacritty/themes/"
+            themes = [f for f in listdir(path) if isfile(join(path, f))]
+        except FileNotFoundError:
+            raise FileNotFoundError("Can't find themes")
+ 
+    themesstr: str = ""
+    for theme in themes:
+        themesstr+=theme.strip().replace(".toml","")+"\n"
+    return themesstr
 
